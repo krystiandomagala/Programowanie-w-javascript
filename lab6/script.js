026cef72e9f47b69d0d7076c069c103fff91dad0
krystiandomagala <79994186+krystiandomagala@users.noreply.github.com>
2022-11-24 18:12:10 +0100

    field

--- lab6/script.js
+++ lab6/script.js
@@ -3,14 +3,47 @@
 let alpha = document.querySelector("#alpha");
 let beta = document.querySelector("#beta");
 let gamma = document.querySelector("#gamma");
+const ball = document.querySelector(".ball");
+const garden = document.querySelector(".garden");
+
+const maxX = garden.clientWidth - ball.clientWidth;
+const maxY = garden.clientHeight - ball.clientHeight;
+
+// get position values
 
 function onDeviceMove(event) {
     console.log(event)
     alpha.innerHTML = (event.alpha).toFixed(2);
     beta.innerHTML = (event.beta).toFixed(2);
     gamma.innerHTML = (event.gamma).toFixed(2);
+
+    let x = event.beta; // In degree in the range [-180,180)
+    let y = event.gamma; // In degree in the range [-90,90)
+  
+    // Because we don't want to have the device upside down
+    // We constrain the x value to the range [-90,90]
+    if (x > 90) {
+      x = 90;
+    }
+    if (x < -90) {
+      x = -90;
+    }
+  
+    // To make computation easier we shift the range of
+    // x and y to [0,180]
+    x += 90;
+    y += 90;
+  
+    // 10 is half the size of the ball
+    // It center the positioning point to the center of the ball
+    ball.style.top = `${(maxY * y) / 180 - 10}px`;
+    ball.style.left = `${(maxX * x) / 180 - 10}px`;
 }
 
+const btn = document.getElementById( "request" );
+btn.addEventListener( "click", permission );
+
+// get permission to use orientation
 
 function permission() {
     if (typeof DeviceMotionEvent.requestPermission === 'function') {
@@ -29,5 +62,3 @@ function permission() {
       window.addEventListener('deviceorientation', onDeviceMove);
     }
   }
-    const btn = document.getElementById( "request" );
-    btn.addEventListener( "click", permission );
