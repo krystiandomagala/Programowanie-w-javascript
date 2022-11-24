b1c70b3867a806dc3a15a3c706eba41d3f034338
krystiandomagala <79994186+krystiandomagala@users.noreply.github.com>
2022-11-24 16:35:49 +0100

    request

--- lab6/script.js
+++ lab6/script.js
@@ -11,19 +11,25 @@ function onDeviceMove(event) {
     gamma.innerHTML = event.gamma;
 }
 
-function permission(){
-
-DeviceOrientationEvent.requestPermission()
-.then(response => {
-  if (response == 'granted') {
-    window.addEventListener('deviceorientation', (e) => {
-      // do something with e
-    })
-  }
-})
-.catch(console.error)
-
-}
-
+if ( location.protocol != "http:" ) {
+    location.href = "http:" + window.location.href.substring( window.location.protocol.length );
+    }
+    function permission () {
+        if ( typeof( DeviceMotionEvent ) !== "undefined" && typeof( DeviceMotionEvent.requestPermission ) === "function" ) {
+            // (optional) Do something before API request prompt.
+            DeviceMotionEvent.requestPermission()
+                .then( response => {
+                // (optional) Do something after API prompt dismissed.
+                if ( response == "granted" ) {
+                    window.addEventListener( "devicemotion", (e) => {
+                        // do something for 'e' here.
+                    })
+                }
+            })
+                .catch( console.error )
+        } else {
+            alert( "DeviceMotionEvent is not defined" );
+        }
+    }
     const btn = document.getElementById( "request" );
     btn.addEventListener( "click", permission );
