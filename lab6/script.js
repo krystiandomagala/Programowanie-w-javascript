e127c9a7b1614485fa4ab6e660c9e29eb7f22809
krystiandomagala <79994186+krystiandomagala@users.noreply.github.com>
2022-11-24 18:21:11 +0100

    disable rotation

--- lab6/script.js
+++ lab6/script.js
@@ -12,6 +12,10 @@ const maxY = garden.clientHeight - ball.clientHeight;
 // get position values
 
 function onDeviceMove(event) {
+
+    if (window.innerHeight > window.innerWidth)
+        document.getElementsByTagName('body')[0].style.transform = "rotate(90deg)";
+
     console.log(event)
     alpha.innerHTML = (event.alpha).toFixed(2);
     beta.innerHTML = (event.beta).toFixed(2);
