875d4045a6930f41a743abc7279de73c9a7cfc8a
krystiandomagala <79994186+krystiandomagala@users.noreply.github.com>
2022-11-25 16:30:51 +0100

    redesigning

--- lab6/script.js
+++ lab6/script.js
@@ -1,65 +1,84 @@
-
-
 let alpha = document.querySelector("#alpha");
 let beta = document.querySelector("#beta");
 let gamma = document.querySelector("#gamma");
-const ball = document.querySelector(".ball");
-const garden = document.querySelector(".garden");
 
-const maxX = garden.clientWidth - ball.clientWidth;
-const maxY = garden.clientHeight - ball.clientHeight;
-
-// get position values
+const ballContainer = document.querySelector("#ball");
+const holeContainer = document.querySelector(".hole");
+const gameFieldContainer = document.querySelector(".game-field");
 
 function onDeviceMove(event) {
-
-    console.log(event)
-    alpha.innerHTML = (event.alpha).toFixed(2);
-    beta.innerHTML = (event.beta).toFixed(2);
-    gamma.innerHTML = (event.gamma).toFixed(2);
-
-    let x = event.gamma; // In degree in the range [-180,180)
-    let y = event.beta; // In degree in the range [-90,90)
-  
-    // Because we don't want to have the device upside down
-    // We constrain the x value to the range [-90,90]
-    if (x > 90) {
-      x = 90;
-    }
-    if (x < -90) {
-      x = -90;
-    }
-  
-    // To make computation easier we shift the range of
-    // x and y to [0,180]
-    x += 90;
-    y += 90;
-  
-    // 10 is half the size of the ball
-    // It center the positioning point to the center of the ball
-    ball.style.top = `${(maxY * y) / 180 - 10}px`;
-    ball.style.left = `${(maxX * x) / 180 - 10}px`;
+  alpha.innerHTML = Math.round(event.alpha);
+  beta.innerHTML = Math.round(event.beta);
+  gamma.innerHTML = Math.round(event.gamma);
 }
 
-const btn = document.getElementById( "request" );
-btn.addEventListener( "click", permission );
+const hole = {
+  x: 0,
+  y: 0
+};
 
-// get permission to use orientation
+const ball = {
+    x: 0,
+    y: 0
+  };
+  
 
-function permission() {
-    if (typeof DeviceMotionEvent.requestPermission === 'function') {
-      // Handle iOS 13+ devices.
-      DeviceMotionEvent.requestPermission()
-        .then((state) => {
-          if (state === 'granted') {
-            window.addEventListener('deviceorientation', onDeviceMove);
-          } else {
-            console.error('Request to access the orientation was rejected');
-          }
-        })
-        .catch(console.error);
-    } else {
-      // Handle regular non iOS 13+ devices.
-      window.addEventListener('deviceorientation', onDeviceMove);
-    }
+function getRandomCoordinate(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function initializeGame() {
+  btn.style.display = "none";
+  ballContainer.style.display = "block";
+  holeContainer.style.display = "block";
+
+  // display hole randomly
+  const MaxX = gameFieldContainer.clientWidth - holeContainer.clientWidth + 18;
+  const MaxY = gameFieldContainer.clientHeight - holeContainer.clientHeight + 158;
+
+  console.log("maxY:" + MaxY);
+  console.log("maxX:" + MaxX);
+
+  //setting position
+  const holeX = holeContainer.style.left = `${getRandomCoordinate(18, MaxX)}px`;
+  const holeY = holeContainer.style.top = `${getRandomCoordinate(158, MaxY)}px`;
+
+  const ballX = ballContainer.getBoundingClientRect().left + 'px';
+  const ballY = ballContainer.getBoundingClientRect().top + 'px';
+  
+  hole.x = holeX;
+  hole.y = holeY;
+
+  ball.x = ballX;
+  ball.y = ballY;
+
+  console.log(hole);
+  console.log(ball);
+}
+
+
+// Request mobile user
+
+const btn = document.getElementById("request");
+btn.addEventListener("click", getPermission);
+function getPermission() {
+  if (typeof DeviceMotionEvent.requestPermission === "function") {
+    // Handle iOS 13+ devices.
+    DeviceMotionEvent.requestPermission()
+      .then((state) => {
+        if (state === "granted") {
+          window.addEventListener("deviceorientation", onDeviceMove);
+          initializeGame();
+        } else {
+          console.error("Request to access the orientation was rejected");
+        }
+      })
+      .catch(console.error);
+  } else {
+    // Handle regular non iOS 13+ devices.
+    window.addEventListener("deviceorientation", onDeviceMove);
+    initializeGame();
   }
+}
